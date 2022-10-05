d20c80a49b432ad1a73121fa29efe696384b979e
lucas-mior <lucas.mior@tutamail.com>
2022-10-05 12:21:04 -0300

    format code

--- janjao/1_2resolvidas.py
+++ janjao/1_2resolvidas.py
@@ -22,7 +22,7 @@ coef_fcj = [['1', '371.936', '0.00193242', '-8.530000e-8', '2.38e-12', '-2.62e-1
 coef_perda_hidraulica = [['H', '7.5e-6', '2.2e-5', '5.0e-6']]
 
 coef_funcao_rendimento_hidraulico = [['1', '0.3587300', '0.0023513', '0.0036111', '0.0000081', '-0.0000049', '-0.00003120'],
-                                     ['2', '0.2518621', '0.0028912', '0.0065000','0.0000186', '-0.0000092', '-0.00005650'],
+                                     ['2', '0.2518621', '0.0028912', '0.0065000', '0.0000186', '-0.0000092', '-0.00005650'],
                                      ['3', '-6.65554162', '0.03689946', '0', '0', '-0.00004493', '0']]
 
 dados_afluencias = [['1', '1.15', '-300', '300'],
@@ -71,11 +71,13 @@ df9 = pd.DataFrame(dados_geracao_eolica, columns=['Cp', 'AR(m^2)'])
 df10 = pd.DataFrame(dados_demanda_em_cada_estagio, columns=['t', 'Lt'])
 df11 = pd.DataFrame(vazao_turbinada_para_cada_usina, columns=['Estágio', 'H1', 'H2', 'H3'])
 
-################################### QUESTÃO 1 ###################################
+# QUESTÃO 1 ###################################
 print('################################### QUESTÃO 1 ###################################')
-#USINAS: 0,1,2 (começa no 0 devido ao index da matriz do pandas)
-#AFLUENCIAS INICIAIS: 1500,1000,900 (valores fornecidos pelo professor)
-def afluenciahidro(usina,afluencia_inicial):
+# USINAS: 0,1,2 (começa no 0 devido ao index da matriz do pandas)
+# AFLUENCIAS INICIAIS: 1500,1000,900 (valores fornecidos pelo professor)
+
+
+def afluenciahidro(usina, afluencia_inicial):
     afluencialista = []
     phi = df7.iloc[usina, 1]
     a = df7.iloc[usina, 2]
@@ -91,14 +93,13 @@ def afluenciahidro(usina,afluencia_inicial):
     print(f'afluenciahidro {usina+1}: {afluencialista}')
     return afluencialista
 
+
 afluencia_1 = afluenciahidro(0, 1500)
 afluencia_2 = afluenciahidro(1, 1000)
 afluencia_3 = afluenciahidro(2, 900)
 
-#VELOCIDADE DO VENTO INICIAL: 25 (valor fornecido pelo professor)
-#massa especifica do ar: 1225
-velocidadeventolista = []
-ro = 1225
+velocidadeventolista = []  # VELOCIDADE DO VENTO INICIAL: 25 (fornecido)
+ro = 1225  # massa especifica do ar: 1225
 phi = float(df8.iloc[0, 0])
 media = int(df8.iloc[0, 1])
 desviopadrao = int(df8.iloc[0, 2])
@@ -132,8 +133,8 @@ for i in range(12):
 print(f'potencia do aero: {potenciaventolista}')
 print(f'potencia total do parque: {potenciatotalventolista}')
 
-################################### QUESTÃO 2 ###################################
-print('################################### QUESTÃO 2 ###################################')
+# QUESTÃO 2 ###################################
+print('######################### QUESTÃO 2 ###########################')
 print('########### HIDRELÉTRICA 1 ###############')
 # AFLUENCIAS INICIAIS: 1500,1000,900 (valores fornecidos pelo professor)
 vol_min = int(df2.iloc[0, 2])
@@ -197,12 +198,22 @@ g4 = float(df4.iloc[0, 5])
 
 for i in range(12):
     q = int(df11.iloc[i, 1])
-    fcm = f0 + f1*volume_medio_lista_1[i] + (f2*(volume_medio_lista_1[i])**2) + (f3*(volume_medio_lista_1[i])**3) + (f4*(volume_medio_lista_1[i])**4)
+    f11 = f1*volume_medio_lista_1[i]
+    f22 = f2*(volume_medio_lista_1[i])**2
+    f33 = (f3*(volume_medio_lista_1[i])**3)
+    f44 = (f4*(volume_medio_lista_1[i])**4)
+    fcm = f0 + f11 + f22 + f33 + f44
     fcm = round(fcm, 2)
     fcm_lista_1.append(fcm)
-    fcj = g0 + g1*(q+vazao_vertida_1[i]) + (g2*(q+vazao_vertida_1[i])**2) + (g3*(q+vazao_vertida_1[i])**3) + (g4*(q+vazao_vertida_1[i])**4)
+
+    g11 = g1*(q+vazao_vertida_1[i])
+    g22 = (g2*(q+vazao_vertida_1[i])**2)
+    g33 = (g3*(q+vazao_vertida_1[i])**3)
+    g44 = (g4*(q+vazao_vertida_1[i])**4)
+    fcj = g0 + g11 + g22 + g33 + g44
     fcj = round(fcj, 2)
     fcj_lista_1.append(fcj)
+
     hb = fcm - fcj
     hb = round(hb, 2)
     hb_lista_1.append(hb)
@@ -210,7 +221,7 @@ print(f'fcm_lista_1: {fcm_lista_1}')
 print(f'fcj_lista_1: {fcj_lista_1}')
 print(f'hb_lista_1: {hb_lista_1}')
 
-########### HIDRELÉTRICA 2 ###############
+# HIDRELÉTRICA 2 ###############
 print('########### HIDRELÉTRICA 2 ###############')
 vol_min = int(df2.iloc[1, 2])
 vol_max = int(df2.iloc[1, 3])
@@ -276,12 +287,22 @@ g4 = float(df4.iloc[1, 5])
 
 for i in range(12):
     q = int(df11.iloc[i, 2])
-    fcm = f0 + f1*volume_medio_lista_2[i] + (f2*(volume_medio_lista_2[i])**2) + (f3*(volume_medio_lista_2[i])**3) + (f4*(volume_medio_lista_2[i])**4)
+    f11 = f1*volume_medio_lista_2[i]
+    f22 = (f2*(volume_medio_lista_2[i])**2)
+    f33 = (f3*(volume_medio_lista_2[i])**3)
+    f44 = (f4*(volume_medio_lista_2[i])**4)
+    fcm = f0 + f11 + f22 + f33 + f44
     fcm = round(fcm, 2)
     fcm_lista_2.append(fcm)
-    fcj = g0 + g1*(q+vazao_vertida_2[i]) + (g2*(q+vazao_vertida_2[i])**2) + (g3*(q+vazao_vertida_2[i])**3) + (g4*(q+vazao_vertida_2[i])**4)
+
+    g11 = g1*(q+vazao_vertida_2[i])
+    g22 = (g2*(q+vazao_vertida_2[i])**2)
+    g33 = (g3*(q+vazao_vertida_2[i])**3)
+    g44 = (g4*(q+vazao_vertida_2[i])**4)
+    fcj = g0 + g11 + g22 + g33 + g44
     fcj = round(fcj, 2)
     fcj_lista_2.append(fcj)
+
     hb = fcm - fcj
     hb = round(hb, 2)
     hb_lista_2.append(hb)
@@ -289,7 +310,7 @@ print(f'fcm_lista_2: {fcm_lista_2}')
 print(f'fcj_lista_2: {fcj_lista_2}')
 print(f'hb_lista_2: {hb_lista_2}')
 
-########### HIDRELÉTRICA 3 ###############
+# HIDRELÉTRICA 3 ###############
 print('########### HIDRELÉTRICA 3 ###############')
 vol_min = int(df2.iloc[2, 2])
 vol_max = int(df2.iloc[2, 3])
@@ -356,12 +377,22 @@ g4 = float(df4.iloc[2, 5])
 
 for i in range(12):
     q = int(df11.iloc[i, 3])
-    fcm = f0 +(f1*(volume_medio_lista_3[i]))+(f2*(volume_medio_lista_3[i])**2)+(f3*(volume_medio_lista_3[i])**3)+(f4*(volume_medio_lista_3[i])**4)
+    f11 = (f1*(volume_medio_lista_3[i]))
+    f22 = (f2*(volume_medio_lista_3[i])**2)
+    f33 = (f3*(volume_medio_lista_3[i])**3)
+    f44 = (f4*(volume_medio_lista_3[i])**4)
+    fcm = f0 + f11 + f22 + f33 + f44
     fcm = round(fcm, 2)
     fcm_lista_3.append(fcm)
-    fcj = g0 + (g1*(q+vazao_vertida_3[i]))+(g2*(q+vazao_vertida_3[i])**2)+(g3*(q+vazao_vertida_3[i])**3)+(g4*(q+vazao_vertida_3[i])**4)
+
+    g11 = (g1*(q+vazao_vertida_3[i]))
+    g22 = (g2*(q+vazao_vertida_3[i])**2)
+    g33 = (g3*(q+vazao_vertida_3[i])**3)
+    g44 = (g4*(q+vazao_vertida_3[i])**4)
+    fcj = g0 + g11 + g22 + g33 + g44
     fcj = round(fcj, 2)
     fcj_lista_3.append(fcj)
+
     hb = fcm - fcj
     hb = round(hb, 2)
     hb_lista_3.append(hb)
@@ -370,8 +401,8 @@ print(f'fcm_lista_3: {fcm_lista_3}')
 print(f'fcj_lista_3: {fcj_lista_3}')
 print(f'hb_lista_3: {hb_lista_3}')
 
-################################### QUESTÃO 3 ###################################
-print('################################### QUESTÃO 3 ###################################')
+# QUESTÃO 3 ###################################
+print('####################### QUESTÃO 3 ##########################')
 unidades_1 = int(df2.iloc[0, 5])
 unidades_2 = int(df2.iloc[1, 5])
 unidades_3 = int(df2.iloc[2, 5])
@@ -401,5 +432,10 @@ for i in range(12):
     q = int(df11.iloc[i, 3])
     w_itn_1 = q/unidades_1
     hl_itn_1 = hb_lista_1[i] - h_1*w_itn_1
-    r_itn_1 = i0 + (i1*w_itn_1) + (i2*hl_itn_1) + (i3**w_itn_1*hl_itn_1) + (i4*(w_itn_1**2)) + (i5*(hl_itn_1**2))
+    i11 = (i1*w_itn_1)
+    i22 = (i2*hl_itn_1)
+    i33 = (i3**w_itn_1*hl_itn_1)
+    i44 = (i4*(w_itn_1**2))
+    i55 = (i5*(hl_itn_1**2))
+    r_itn_1 = i0 + i11 + i22 + i33 + i44 + i55
     gh_itn = 0.00981*r_itn_1*hl_itn_1*w_itn_1
